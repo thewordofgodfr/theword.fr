649bda7acb11f89f341c9f81b5a0400873488ad7
TheWord <contact@thewordofgod.fr>
2025-11-27 13:56:53 +0100

    Create zh.ts

---
+++ src/i18n/zh.ts
@@ -0,0 +1,8 @@
+// src/i18n/zh.ts
+import type { TranslationDict } from './types';
+
+const zh: TranslationDict = {
+  // === colle ici tout le contenu de ton const enTranslations = { ... } ===
+};
+
+export default zh;
