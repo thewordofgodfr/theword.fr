783e2dbc8faa2fac823cd280668d928959e45f58
TheWord <contact@thewordofgod.fr>
2025-11-27 14:05:29 +0100

    Create ko.ts

---
+++ src/i18n/ko.ts
@@ -0,0 +1,8 @@
+// src/i18n/ko.ts
+import type { TranslationDict } from './types';
+
+const ko: TranslationDict = {
+  // === colle ici tout le contenu de ton const enTranslations = { ... } ===
+};
+
+export default ko;
