6f454932c59060197c116b4a7a2d9962bb3d51b9
TheWord <contact@thewordofgod.fr>
2025-11-27 13:55:24 +0100

    Create hi.ts

---
+++ src/i18n/hi.ts
@@ -0,0 +1,9 @@
+// src/i18n/hi.ts
+import type { TranslationDict } from './types';
+
+// 👉 colle ici ton objet enTranslations en le renommant "en"
+const hi: TranslationDict = {
+  // === colle ici tout le contenu de ton const enTranslations = { ... } ===
+};
+
+export default hi;
