e96f1600cb94b97f54bf54a63939a3d5ce7d26c8
TheWord <contact@thewordofgod.fr>
2025-11-27 14:07:52 +0100

    Create ja.ts

---
+++ src/i18n/ja.ts
@@ -0,0 +1,8 @@
+// src/i18n/ja.ts
+import type { TranslationDict } from './types';
+
+const ja: TranslationDict = {
+  // === colle ici tout le contenu de ton const enTranslations = { ... } ===
+};
+
+export default ja;
