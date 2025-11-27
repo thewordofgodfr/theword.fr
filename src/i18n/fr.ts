63a9eeb19360bb351bd4fc9ad159ec21b47ca9dd
TheWord <contact@thewordofgod.fr>
2025-11-27 14:10:39 +0100

    Update fr.ts

--- src/i18n/fr.ts
+++ src/i18n/fr.ts
@@ -1,8 +1,6 @@
 // src/i18n/fr.ts
 import type { TranslationDict } from './types';
 
-// 👉 Ici tu colles TON objet frTranslations actuel,
-//    en remplaçant juste le nom par "fr" et en enlevant "const frTranslations =".
 const fr: TranslationDict = {
   // Navigation
   home: 'Accueil',
