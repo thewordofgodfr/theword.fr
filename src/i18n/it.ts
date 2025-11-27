0b672cd807c912872340da081b4f9bf798739de7
TheWord <contact@thewordofgod.fr>
2025-11-27 13:50:32 +0100

    Create it.ts

---
+++ src/i18n/it.ts
@@ -0,0 +1,9 @@
+// src/i18n/it.ts
+import type { TranslationDict } from './types';
+
+// 👉 colle ici ton objet enTranslations en le renommant "en"
+const it: TranslationDict = {
+  // === colle ici tout le contenu de ton const enTranslations = { ... } ===
+};
+
+export default it;
