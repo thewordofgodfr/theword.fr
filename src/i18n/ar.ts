dc8ccbc87c1093ebe0f9a5de8f1c52f008b1825b
TheWord <contact@thewordofgod.fr>
2025-11-27 13:59:13 +0100

    Create ar.ts

---
+++ src/i18n/ar.ts
@@ -0,0 +1,8 @@
+// src/i18n/ar.ts
+import type { TranslationDict } from './types';
+
+const ar: TranslationDict = {
+  // === colle ici tout le contenu de ton const enTranslations = { ... } ===
+};
+
+export default ar;
