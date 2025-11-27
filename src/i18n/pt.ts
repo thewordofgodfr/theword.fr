72d430d75cd5134f21bcb9b4e99a773dac657f2e
TheWord <contact@thewordofgod.fr>
2025-11-27 13:53:42 +0100

    Create pt.ts

---
+++ src/i18n/pt.ts
@@ -0,0 +1,9 @@
+// src/i18n/pt.ts
+import type { TranslationDict } from './types';
+
+// 👉 colle ici ton objet enTranslations en le renommant "en"
+const pt: TranslationDict = {
+  // === colle ici tout le contenu de ton const enTranslations = { ... } ===
+};
+
+export default pt;
