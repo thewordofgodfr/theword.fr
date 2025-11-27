437d28cc52e55a845cacc439becdac3a7f7eaefa
TheWord <contact@thewordofgod.fr>
2025-11-27 13:47:48 +0100

    Create ru.ts

---
+++ src/i18n/ru.ts
@@ -0,0 +1,9 @@
+// src/i18n/ru.ts
+import type { TranslationDict } from './types';
+
+// 👉 colle ici ton objet enTranslations en le renommant "en"
+const ru: TranslationDict = {
+  // === colle ici tout le contenu de ton const enTranslations = { ... } ===
+};
+
+export default ru;
