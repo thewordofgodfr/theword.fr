62a41f1e89bb496544a09df2324638036f7b510d
TheWord <contact@thewordofgod.fr>
2025-11-27 14:08:59 +0100

    Create yo.ts

---
+++ src/i18n/yo.ts
@@ -0,0 +1,8 @@
+// src/i18n/yo.ts
+import type { TranslationDict } from './types';
+
+const yo: TranslationDict = {
+  // === colle ici tout le contenu de ton const enTranslations = { ... } ===
+};
+
+export default yo;
