8d8107f3f1b31186e66756304f57c1069d8f3afc
TheWord <contact@thewordofgod.fr>
2025-11-27 14:00:53 +0100

    Create id.ts

---
+++ src/i18n/id.ts
@@ -0,0 +1,8 @@
+// src/i18n/id.ts
+import type { TranslationDict } from './types';
+
+const id: TranslationDict = {
+  // === colle ici tout le contenu de ton const enTranslations = { ... } ===
+};
+
+export default id;
