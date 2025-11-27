13a3d859b5a5b2e9fd600985d5946b8f638bb641
TheWord <contact@thewordofgod.fr>
2025-11-27 14:09:57 +0100

    Create tr.ts

---
+++ src/i18n/tr.ts
@@ -0,0 +1,8 @@
+// src/i18n/tr.ts
+import type { TranslationDict } from './types';
+
+const tr: TranslationDict = {
+  // === colle ici tout le contenu de ton const enTranslations = { ... } ===
+};
+
+export default tr;
