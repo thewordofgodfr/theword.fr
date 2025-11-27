5818c78fcde408266608678cd88706eb88471c09
TheWord <contact@thewordofgod.fr>
2025-11-27 14:03:43 +0100

    Create sw.ts

---
+++ src/i18n/sw.ts
@@ -0,0 +1,8 @@
+// src/i18n/sw.ts
+import type { TranslationDict } from './types';
+
+const sw: TranslationDict = {
+  // === colle ici tout le contenu de ton const enTranslations = { ... } ===
+};
+
+export default sw;
