b9e6febd617c9f69892228b4b9f5233282fb0fff
TheWord <contact@thewordofgod.fr>
2026-09-20 21:50:38 +0200

    Update French versions footnote for clarity
    Removed extra text from the versions footnote in French localization.

--- src/i18n/fr.ts
+++ src/i18n/fr.ts
@@ -240,7 +240,7 @@ Jean 8:31-32`,
   notesPoint3: 'Renommez vos listes, copiez/partagez.',
   createdWithLove: 'Créé avec amour pour répandre la Parole de Dieu',
   versionsFootnote:
-    'À propos des textes : les Bibles intégrées sont utilisées selon leurs licences respectives. Seule la Louis Segond 1910 a été modernisée en 2025 (grammaire et vocabulaire), dans le strict respect des manuscrits originaux .                  Si vous souhaitez l’application Android merci de m’envoyer une demande par e-mail et je vous enverrai un lien (application en version test).',
+    'À propos des textes : les Bibles intégrées sont utilisées selon leurs licences respectives. Seule la Louis Segond 1910 a été modernisée en 2025 (grammaire et vocabulaire), dans le strict respect des manuscrits originaux .',
 
   // Quick slots
   quickSlotsIntro:
